54c34362cf3509666e320a7b6b4ba8541904b71b
George Ashworth <gashwor1@mail.ccsf.edu>
2021-07-28 23:02:37 -0700

    Incremental improvements on wall hugging.

--- rh_wall_hug/rh_wall_hug.ino
+++ rh_wall_hug/rh_wall_hug.ino
@@ -13,18 +13,22 @@ const int sensorPin0 = 0;
 const int sensorPin1 = 1;
 const int sensorPin2 = 2;
 
-const int x0 = 10;
+const int x_min = 8;
+const int x0 = 18;
 const int y_min = 12;
 const int y_max = 16;
-const int y0 = 18;
+const int y0 = 30;
 
 
 Servo servoRight;
 Servo servoLeft;
 
-SharpIR sensor0 = SharpIR(sensorPin0, model);
-SharpIR sensor1 = SharpIR(sensorPin1, model);
-SharpIR sensor2 = SharpIR(sensorPin2, model);
+SharpIR sensor0 = SharpIR( SharpIR::GP2Y0A21YK0F, A0 );
+SharpIR sensor1 = SharpIR( SharpIR::GP2Y0A21YK0F, A1 );
+SharpIR sensor2 = SharpIR( SharpIR::GP2Y0A21YK0F, A2 );
+// SharpIR sensor0 = SharpIR(sensorPin0, model);
+// SharpIR sensor1 = SharpIR(sensorPin1, model);
+// SharpIR sensor2 = SharpIR(sensorPin2, model);
 
 
 void setup() {
@@ -43,7 +47,23 @@ void loop()
   int distance1 = sensor1.getDistance();
   int distance2 = sensor2.getDistance();
 
+  Serial.print("Sensor 0: ");
+  Serial.print(distance0);
 
+  Serial.print("    Sensor 1: ");
+  Serial.print(distance1);
+
+  Serial.print("    Sensor 2: ");
+  Serial.println(distance2);
+
+  /*
+  if front sensor is very close to wall:
+    back up */
+  if (distance1 <= x_min)
+  {
+    servoRight.write(109);
+    servoLeft.write(78); 
+  }
 
   /*
   if front sensor and right sensor are farther than contact length
@@ -51,8 +71,8 @@ void loop()
     drive straight */
   if (distance1 >= x0 && distance0 >= y0)
   {
-    servoRight.write(68);
-    servoLeft.write(130);
+    servoRight.write(74);
+    servoLeft.write(118);
   }
 
   /*
@@ -60,55 +80,72 @@ void loop()
     turn hard left until right sensor is less than min */
   if (distance1 <= x0 && distance0 >= y0)
   {
-    while (distance0 >= y_min)
-    {
-      servoRight.write(60);
-      servoLeft.write(90);
-    }
+    Serial.println("****** HARD LEFT ******");
+    // while (distance0 >= y_min)
+    // {
+    //   servoRight.write(60);
+    //   servoLeft.write(90);
+    // }
+    servoRight.write(66);
+    servoLeft.write(90);
+    delay(200);
+
   }
 
-  // /*
-  // if right sensor makes contact, but is greater than max:
-  //   turn forward-right until less than min */
-  // if (distance0 <= y0 && distance0 >= y_max)
-  // {
-  //   while (distance0 >= y_max)
-  //   {
-  //     servoRight.write(73);
-  //     servoLeft.write(130);
-  //   }
-  // }
-
-  // /*
-  // if right sensor makes contact, but is less than min:
-  //   turn forward-left until past min */
-  // if (distance0 <= y0 && distance0 <= y_min)
-  // {
-  //   while (distance0 <= y_min)
-  //   {
-  //     servoRight.write(60);
-  //     servoLeft.write(120);
-  //   }
-  // }  
-
-  // /*
-  // if front sensor makes contact, and right sensor makes contact:
-  //   turn hard left until right sensor is greater than max */
-  // if (distance1 <= x0 && distance0 <= y0)
-  // {
-  //   while (distance0 <= y_max)
-  //   {
-  //     servoRight.write(60);
-  //     servoLeft.write(120);
-  //   }
-  // } 
-
-  else
+  /*
+  if right sensor makes contact, but is greater than max:
+    turn forward-right until less than min */
+  if (distance0 <= y0 && distance0 >= y_max)
   {
-    servoRight.write(68);
-    servoLeft.write(130);
+    Serial.println("****** SOFT RIGHT ******");
+    // while (distance0 >= y_max)
+    // {
+    //   servoRight.write(73);
+    //   servoLeft.write(130);
+    // }
+    servoRight.write(79);
+    servoLeft.write(118);
+    delay(200);
   }
 
-  delay(1000);
+  /*
+  if right sensor makes contact, but is less than min:
+    turn forward-left until past min */
+  if (distance0 <= y0 && distance0 <= y_min)
+  {
+    Serial.println("****** SOFT LEFT ******");    
+    // while (distance0 <= y_min)
+    // {
+    //   servoRight.write(60);
+    //   servoLeft.write(120);
+    // }
+    servoRight.write(74);
+    servoLeft.write(112);
+    delay(200);
+  }  
+
+  /*
+  if front sensor makes contact, and right sensor makes contact:
+    turn hard left until right sensor is greater than max */
+  if (distance1 <= x0 && distance0 <= y0)
+  {
+    Serial.println("****** HARD LEFT CORNER ******");    
+    // while (distance0 <= y_max)
+    // {
+    //   servoRight.write(60);
+    //   servoLeft.write(120);
+    // }
+    servoRight.write(66);
+    servoLeft.write(108);
+    delay(750);
+  } 
+
+  // else
+  // {
+  //   servoRight.write(68);
+  //   servoLeft.write(130);
+  // }
+;
+  delay(250);
 
 }
